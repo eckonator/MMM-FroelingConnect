8ee8246ec46da8d622d0726920eb8f4ea1edbf4c
Markus Eckert <me@stilgrenze.de>
2022-03-15 10:19:41 +0100

    Bugfix

--- node_helper.js
+++ node_helper.js
@@ -10,43 +10,38 @@ module.exports = NodeHelper.create({
 	updateInterval : null,
 	refreshTokenInterval : null,
 	session : {},
-	loginState: false,
 
 	login: async function(payload) {
 		var self = this;
 		if(Object.keys(self.componentStates).length > 0 && self.componentStates.constructor === Object) {
 			self.sendSocketNotification("MMM-FroelingConnect-newCompontentState", self.componentStates);
 		};
-		if(self.loginState === false) {
-			self.requestClient({
-				method: "post",
-				url: "https://connect-api.froeling.com/app/v1.0/resources/loginNew",
-				headers: {
-					"Content-Type": "application/json",
-					Connection: "keep-alive",
-					Accept: "*/*",
-					"User-Agent": "Froeling PROD/2107.1 (com.froeling.connect-ios; build:2107.1.01; iOS 14.8.0) Alamofire/4.8.1",
-					"Accept-Language": "de",
-				},
-				data: JSON.stringify({
-					osType: "IOS",
-					userName: payload.username,
-					password: payload.password,
-				}),
-			}).then(function (response) {
-				// console.log('MMM-FroelingConnect: ' + response.data);
-				// console.log('MMM-FroelingConnect: ' + response.status);
-				// console.log('MMM-FroelingConnect: ' + response.statusText);
-				// console.log('MMM-FroelingConnect: ' + response.headers);
-				// console.log('MMM-FroelingConnect: ' + response.config);
-				//console.log('MMM-FroelingConnect: ' + response.headers["authorization"]);
-				self.loginState = true;
-				self.sendSocketNotification("MMM-FroelingConnect-Login-OK", {session: response.data, token: response.headers["authorization"], interval: payload.interval});
-			}).catch(function (error) {
-				self.loginState = false;
-				console.log(error);
-			});
-		}
+		self.requestClient({
+			method: "post",
+			url: "https://connect-api.froeling.com/app/v1.0/resources/loginNew",
+			headers: {
+				"Content-Type": "application/json",
+				Connection: "keep-alive",
+				Accept: "*/*",
+				"User-Agent": "Froeling PROD/2107.1 (com.froeling.connect-ios; build:2107.1.01; iOS 14.8.0) Alamofire/4.8.1",
+				"Accept-Language": "de",
+			},
+			data: JSON.stringify({
+				osType: "IOS",
+				userName: payload.username,
+				password: payload.password,
+			}),
+		}).then(function (response) {
+			// console.log('MMM-FroelingConnect: ' + response.data);
+			// console.log('MMM-FroelingConnect: ' + response.status);
+			// console.log('MMM-FroelingConnect: ' + response.statusText);
+			// console.log('MMM-FroelingConnect: ' + response.headers);
+			// console.log('MMM-FroelingConnect: ' + response.config);
+			//console.log('MMM-FroelingConnect: ' + response.headers["authorization"]);
+			self.sendSocketNotification("MMM-FroelingConnect-Login-OK", {session: response.data, token: response.headers["authorization"], interval: payload.interval});
+		}).catch(function (error) {
+			console.log(error);
+		});
 	},
 
 	bootup: async function(payload) {
@@ -65,7 +60,6 @@ module.exports = NodeHelper.create({
 			}, payload.interval * 60 * 1000);
 			//}, 20 * 1000);
 			self.refreshTokenInterval = setInterval(() => {
-				self.loginState = false;
 				self.login();
 			}, 60 * 60 * 1000);
 		}
